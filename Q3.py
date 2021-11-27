71763153134d506f9c1289d7790ce837919acc3e
林峻毅 <CherryBlossom@linjunyide-MacBook-Pro.local>
2021-11-27 23:19:42 +0800

    finish Q3 Q4

--- Q3.py
+++ Q3.py
@@ -3,30 +3,107 @@ import os
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import size
 
-def imgshow(title, img):
-    pass
+def imgshow(title, pic):
+    cv2.imshow(title, pic)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def create_Gaussian(s, sigma):
-    pass
 
-def conv2d(img, ft, stride=1):
-    pass
+    s_mid = (s[0] - 1) // 2
+    f = np.zeros(s)
+    total = 0.0
+    
+    # calculate the Gaussian Filter
+    for y in range(-s_mid, s_mid + 1):
+        for x in range(-s_mid, s_mid + 1):
+            f[y + s_mid][x + s_mid] = math.exp((-(y ** 2 + x ** 2)) ) / (2.0 * math.pi * (sigma ** 2))
+            total += f[y + s_mid][x + s_mid]
+
+    for y in range(len(f)):
+        for x in range(len(f[y])):
+            f[y][x] = f[y][x] / total
+
+    return f
+
+
+def conv2d(img, filter, stride=1):
+    row, col = img.shape[0], img.shape[1]
+    ft_mid = (filter.shape[0] - 1) // 2
+    print('test')
+    pad_img = np.pad(array = img, pad_width = ((ft_mid, ft_mid),(ft_mid, ft_mid)), mode='constant', constant_values=0)
+    print('test finish')
+    result = np.zeros((row, col))
+    for r in range(ft_mid, row + ft_mid, stride):
+        for c in range(ft_mid, col + ft_mid, stride):
+            result[r - ft_mid][c - ft_mid] = np.sum(pad_img[r - ft_mid:r + ft_mid + 1, c - ft_mid:c + ft_mid + 1] * filter)
+
+    return result
+
 
 def Gaussian_blur():
-    pass
+    # file_path = os.path.join('Dataset_opencvdl', 'Q3_Image', 'Chihiro.jpg')
+    pic = cv2.imread('./Q3_image/House.jpg')
+    imgshow('original', pic)
+    filter = create_Gaussian((3, 3), 1.5)
+    gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    imgshow('RGB to gray', gray)
+
+    result = conv2d(gray, filter)
+    result = result.astype('uint8')
+    imgshow('Gaussian blur', result)
+    return result
 
 def Sobel_x():
-    pass
+    # file_path = os.path.join('Dataset_opencvdl', 'Q3_Image', 'Chihiro.jpg')
+    pic = cv2.imread('./Q3_image/House.jpg')
+    gaussian_filter = create_Gaussian((3, 3), 1.5)
+    gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    result = conv2d(gray, gaussian_filter)
+    sobelx_filter = np.array(
+        [
+            [-1.0, 0.0, 1.0],
+            [-2.0, 0.0, 2.0],
+            [-1.0, 0.0, 1.0]
+        ]
+    )
+    sobelx_result = conv2d(result, sobelx_filter)
+    sobelx_result = np.abs(sobelx_result)
+    imgshow('sobelx_result', sobelx_result/255)
+    return sobelx_result/255
 
 def Sobel_y():
-    pass
+    # file_path = os.path.join('Dataset_opencvdl', 'Q3_Image', 'Chihiro.jpg')
+    pic = cv2.imread('./Q3_image/House.jpg')
+    gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    gaussian_filter = create_Gaussian((3, 3), 1.5)
+    g_result = conv2d(gray, gaussian_filter)
+
+    sobely_filter = np.array(
+        [
+            [1.0, 2.0, 1.0],
+            [0.0, 0.0, 0.0],
+            [-1.0, -2.0, -1.0]
+        ]
+    )
+    sobely_result = conv2d(g_result, sobely_filter)
+    sobely_result = np.abs(sobely_result)
+    imgshow('sobely_result', sobely_result/255)
+    return sobely_result/255
 
 def Magnitude():
-    pass
+    img1 = Sobel_x()
+    img2 = Sobel_y()
+    mag = img1 ** 2 + img2 ** 2
+    mag = mag ** 0.5
+
+    return mag
 
 if __name__ == "__main__":
+    Gaussian_blur()
+    print('finish Gaussian blur')
     img = Magnitude()
     print(img)
-    imgshow('123', img)
-
+    imgshow('Magnitude', img)
--- Q4.py
+++ Q4.py
@@ -3,7 +3,18 @@ import os
 import numpy as np
 
 def transform(rot=30, scale=0.9, tx=200, ty=300):
-    pass
+    # file_path = os.path.join('Dataset_opencvdl', 'Q4_Image', 'Parrot.png')
+    pic = cv2.imread('./Q4_image/SQUARE-01.png')
+
+    M = cv2.getRotationMatrix2D((160, 84), rot, scale)
+    M[0, 2] += ty
+    M[1, 2] += tx
+    res = cv2.warpAffine(pic, M, (pic.shape[1], pic.shape[0]))
+
+
+    cv2.imshow('transform', res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     transform()
