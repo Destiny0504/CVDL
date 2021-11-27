373cd55d546044d93d930111e8844e8e1727c665
林峻毅 <CherryBlossom@linjunyide-MacBook-Pro.local>
2021-11-28 01:05:01 +0800

    Q4 need sharing

--- Q4.py
+++ Q4.py
@@ -1,18 +1,37 @@
+from math import pi
 import cv2
-import os
 import numpy as np
 
-def transform(rot=30, scale=0.9, tx=200, ty=300):
+def transform(rotate=10, scale=0.5, tx=0, ty=60):
     # file_path = os.path.join('Dataset_opencvdl', 'Q4_Image', 'Parrot.png')
     pic = cv2.imread('./Q4_image/SQUARE-01.png')
+    cv2.imshow('do nothing', pic)
 
-    M = cv2.getRotationMatrix2D((160, 84), rot, scale)
-    M[0, 2] += ty
-    M[1, 2] += tx
-    res = cv2.warpAffine(pic, M, (pic.shape[1], pic.shape[0]))
+    resized_pic = cv2.resize(pic,(256, 256), interpolation=False)
+    cv2.imshow('resize', resized_pic)
 
+    cv2.namedWindow("transform", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("transform", 400, 300)
 
-    cv2.imshow('transform', res)
+    cv2.namedWindow("rotate", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("rotate", 400, 300)
+
+    # cv2.namedWindow("sharing", cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow("sharing", 400, 300)
+
+    translate_matrix = np.float32([[1, 0, tx], [0, 1, ty]])
+    translated = cv2.warpAffine(resized_pic, translate_matrix, (pic.shape[1], pic.shape[0]))
+
+    cv2.imshow('transform', translated)
+
+    rotate = cv2.getRotationMatrix2D((128, 188), rotate, scale)
+    rotated = cv2.warpAffine(translated, rotate, (pic.shape[1], pic.shape[0]))
+
+    cv2.imshow('rotate', rotated)
+
+    sharing_matrix
+
+    #cv2.imshow('transform',res)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
